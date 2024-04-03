27bdd94fd17c0d2ab1b6463155d8378fe14c28e2
Mamadou Ciré DIALLO <macdialloisidk@groupeisi.com>
2024-04-03 18:51:42 +0000

    Initial version of historical generated by generator-jhipster@8.1.0

---
+++ src/main/java/com/che/aop/logging/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Logging aspect.
+ */
+package com.che.aop.logging;
---
+++ src/test/java/com/che/security/jwt/AuthenticationIntegrationTest.java
@@ -0,0 +1,29 @@
+package com.che.security.jwt;
+
+import com.che.config.SecurityConfiguration;
+import com.che.config.SecurityJwtConfiguration;
+import com.che.config.WebConfigurer;
+import com.che.management.SecurityMetersService;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import tech.jhipster.config.JHipsterProperties;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(
+    classes = {
+        JHipsterProperties.class,
+        WebConfigurer.class,
+        SecurityConfiguration.class,
+        SecurityJwtConfiguration.class,
+        SecurityMetersService.class,
+        JwtAuthenticationTestUtils.class,
+    }
+)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface AuthenticationIntegrationTest {
+}
